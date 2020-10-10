72efd19f63e36ca677567800900b8b8efbc52b81
Josue Martinez <josuemartinez3141@gmail.com>
2020-10-09 20:36:00 -0400

    redux boilerplate

--- src/App.js
+++ src/App.js
@@ -1,38 +1,46 @@
-import React, { useState } from 'react';
-import './App.css';
-
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [ author, setAuthor ] = useState(undefined);
-  const [ name, setName ] = useState(undefined);
-  const [ year, setYear ] = useState(undefined);
+  const [author, setAuthor] = useState(undefined);
+  const [name, setName] = useState(undefined);
+  const [year, setYear] = useState(undefined);
 
-  const [ search, setSearch ] = useState(undefined);
+  const [search, setSearch] = useState(undefined);
 
   const handleChange = (e) => {
-    setSearch(e.target.value)
-  } 
+    setSearch(e.target.value);
+  };
 
   function getData(e) {
     fetch(`https://openlibrary.org/search.json?author=${search}`)
-    .then((response) => {
-      return response.json()
-    }).then((data) => {
-      const book = data.docs[Math.floor(Math.random() * data.docs.length)];
-      setAuthor(book.author_alternative_name[0])
-      setName(book.title)
-      setYear(book.first_publish_year)
-    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        const book = data.docs[Math.floor(Math.random() * data.docs.length)];
+        setAuthor(book.author_alternative_name[0]);
+        setName(book.title);
+        setYear(book.first_publish_year);
+      });
     e.preventDefault();
   }
-  
+
   return (
     <div className="container">
       <h1 className="heading">Find a random book by a searched author</h1>
       <form onSubmit={getData}>
-        <input className="input" onChange={handleChange} required placeholder="enter author"/>
-        <br/>
-        <button className="btn" type="submit">Find Book!</button>
+        <input
+          className="input"
+          onChange={handleChange}
+          required
+          placeholder="enter author"
+        />
+        <br />
+        <button className="btn" type="submit">
+          Find Book!
+        </button>
       </form>
       <div className="box">
         <h1>{author}</h1>
